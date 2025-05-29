59e2981003e273843a44f638d440c542abb564a9
Huy Ta <talehuy@hotmail.com>
2025-05-29 10:55:52 +0700

    Delete 16 directory

--- 16/gdk/library_app/migrations/16.6/pre-migrate.py
+++
@@ -1,3 +0,0 @@
-
-def migrate(cr, version):
-    cr.execute('ALTER TABLE library_book RENAME COLUMN date_published TO date_published_char')
--- 16/third_party/product_data_feed_feature/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import product_data_feed_feature
-from . import product_data_feed_feature_value
-from . import product_product
-from . import product_template
