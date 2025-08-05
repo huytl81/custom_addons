601fc7a0bcfff7d79421172895bbce3cee05dfb7
Huy Ta <talehuy81@gmail.com>
2025-08-05 20:38:07 +0700

    methods

--- huyta/real_estate_ads/models/property_offer.py
+++ huyta/real_estate_ads/models/property_offer.py
@@ -8,7 +8,7 @@ class PropertyOffer(models.Model):
 
     price = fields.Float(string='Price')
     validity = fields.Integer(string='Validity (days)')
-#    created_date = fields.Date(string='Created Date', default='_set_created_date')
+#	created_date = fields.Date(string='Created Date', default='_set_created_date')
     created_date = fields.Date(string='Created Date')
     deadline = fields.Date(string='Deadline',compute="_compute_deadline", inverse="_inverse_deadline")
     status = fields.Selection(
@@ -62,19 +62,41 @@ class PropertyOffer(models.Model):
     @api.constrains('validity')
     def _check_validity(self):
         for record in self:
-            if (record.deadline <= record.created_date):
-                raise ValidationError("Created date cannot be greater than or equal Deadline")
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+            if (record.deadline and record.created_date):
+                if (record.deadline <= record.created_date):
+                    raise ValidationError("Created date cannot be greater than or equal Deadline")
+            else:
+                record.validity = False
+    def write(self,vals):
+        print(vals)
+        print(self)
+        print(self.env.cr)
+        print(self.env.uid)
+        print(self.env.context)
+        print(self.env.user)
+        print(self.env.company)
+        print(self.env.registry)
         
+        res_partner = self.env['res.partner'].browse(1)
+        print(res_partner.name)
+        
+        res_partner_counted = self.env['res.partner'].search_count([
+            ('is_company', '=', True)
+        ])
+        print("Total Partners:", res_partner_counted)
+        
+        res_partner_ids = self.env['res.partner'].search(
+            [('is_company', '=', True)],
+            limit=3, order='name desc'
+        )
+        print(res_partner_ids.mapped('name'))
+        
+        print(res_partner_ids.mapped(lambda r: (r.name, r.phone)))
+        
+        res_partner_ids_filtered = self.env['res.partner'].search(
+            [('is_company', '=', True)],
+            limit=3, order='name desc'
+        ).filtered(lambda r: len(r.name) > 50)
+        print(res_partner_ids_filtered.mapped('name'))
+
+        return super(PropertyOffer, self).write(vals)
